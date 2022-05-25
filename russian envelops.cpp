39c5966b87ebe67967fceb588441d44d2c21b1f3
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-05-25 11:56:48 +0530

    600 ms

---
+++ russian envelops.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& E) {
+        sort(E.begin(), E.end(), [](vector<int>& a, vector<int>& b) 
+             -> bool {return a[0] == b[0] ? b[1] < a[1] : a[0] < b[0];});
+        vector<int> dp;
+        for (auto& env : E) {
+            int height = env[1];
+            int left = lower_bound(dp.begin(), dp.end(), height) - dp.begin();
+            if (left == dp.size()) dp.push_back(height);
+            dp[left] = height;
+        }
+        return dp.size();
+    }
+};
