ced85c04196aecd71e82ad1baf9341ae76d67be4
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-05-30 15:09:09 +0530

    0 ms

---
+++ number of 1 bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res=0;
+        while (n){
+            if (n&1){
+                res++;
+            }
+            n = n>>1;
+        }
+        return res;
+    }
+};
