2a08de14145172d793a208368e30d945a53310dc
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-01 10:00:04 +0530

    12 ms

---
+++ running sum of 1d array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        ans[0] = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+            ans[i] = ans[i-1] + nums[i];
+        return ans;
+    }
+};
