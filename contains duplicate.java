6da09c50146d8bb9614564009565ea0d3a9e0d9b
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-02 17:00:35 +0530

    10 ms

---
+++ contains duplicate.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i ++){
+            if (set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }    
+        return false;
+    }
+}
