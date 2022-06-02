b2f979b211183ce65aef27fd8a205d898c09baf8
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-02 17:05:24 +0530

    3 ms

---
+++ container with most water.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] H) {
+        int ans = 0, i = 0, j = H.length-1, res = 0;
+        while (i < j) {
+            if (H[i] <= H[j]) {
+                res = H[i] * (j - i);
+                i++;
+            } else {
+                res = H[j] * (j - i);
+                j--;
+            }
+            if (res > ans) ans = res;
+        }
+        return ans;
+    }
+}
