316bc72682c76cb8d19d3f3cd2898b10dace77a2
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-02 17:06:19 +0530

    89 ms

---
+++ container with most water.js
@@ -0,0 +1,8 @@
+var maxArea = function(H) {
+    let ans = 0, i = 0, j = H.length-1
+    while (i < j) {
+        ans = Math.max(ans, Math.min(H[i], H[j]) * (j - i))
+        H[i] <= H[j] ? i++ : j--
+    }
+    return ans
+};
