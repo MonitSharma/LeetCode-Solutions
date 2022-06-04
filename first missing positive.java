6afe990069935761c79acc5ee9a8dceec64c08a0
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-04 14:42:19 +0530

    4 ms

---
+++ first missing positive.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 1;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+	    // Swap the number until they are in the right position.
+            while (nums[i] >= 1 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
+    }
+}
