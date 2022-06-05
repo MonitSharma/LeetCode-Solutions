2d1f38a7af4c495194a60cd74c1a954ba3cd031f
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-05 17:07:53 +0530

    1 ms

---
+++ n-queens 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    int ans;
+
+    public int totalNQueens(int N) {
+        ans = 0;
+        place(0,0,0,0,N);
+        return ans;
+    }
+
+    private void place(int i, int vert, int ldiag, int rdiag, int N) {
+        if (i == N) ans++;
+        else for (int j = 0; j < N; j++) {
+            int vmask = 1 << j, lmask = 1 << (i+j), rmask = 1 << (N-i-1+j);
+            if ((vert & vmask) + (ldiag & lmask) + (rdiag & rmask) > 0) continue;
+            place(i+1, vert | vmask, ldiag | lmask, rdiag | rmask, N);
+        }
+    }
+}
