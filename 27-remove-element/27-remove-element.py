0524f2fd3796828714ef86340b3465fd38a7a15c
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-09 10:45:19 +0530

    Time: 47 ms (48.22%), Space: 13.9 MB (13.32%) - LeetHub

---
+++ 27-remove-element/27-remove-element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums, val):
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != val :
+                nums[count] = nums[i]
+                count +=1
+        return count
