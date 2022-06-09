38d1d9f53c2c269ef515e96d3d11c2361bbb9615
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-09 09:46:07 +0530

    Time: 134 ms (77.65%), Space: 47.3 MB (95.08%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.js
+++ 778-swim-in-rising-water/778-swim-in-rising-water.js
@@ -1,16 +1,40 @@
 const moves = [[1,0],[0,1],[-1,0],[0,-1]]
 
 var swimInWater = function(grid) {
-    let pq = new MinPriorityQueue(),
-        N = grid.length - 1, ans = grid[0][0], i = 0, j = 0
+    let N = grid.length - 1, ans = grid[0][0], i = 0, j = 0, prio = 0
+
+    // custom Min-Heap implementation
+    let heap = [,]
+    const hpush = val => {
+        let i = heap.length, par = i >> 1, temp
+        heap.push(val)
+        while (heap[par] > heap[i]) {
+            temp = heap[par], heap[par] = heap[i], heap[i] = temp
+            i = par, par = i >> 1
+        }
+    }
+    const hpop = () => {
+        if (heap.length === 1) return null
+        let top = heap[1], left, right, temp,
+            i = 1, child = heap[3] < heap[2] ? 3 : 2
+        if (heap.length > 2) heap[1] = heap.pop()
+        else heap.pop()
+        while (heap[i] > heap[child]) {
+            temp = heap[child], heap[child] = heap[i], heap[i] = temp
+            i = child, left = i << 1, right = left + 1
+            child = heap[right] < heap[left] ? right : left
+        }
+        return top
+    }
+
     while (i < N || j < N) {
         for (let [a,b] of moves) {
             let ia = i + a, jb = j + b
             if (ia < 0 || ia > N || jb < 0 || jb > N || grid[ia][jb] > 2500) continue
-            pq.enqueue((grid[ia][jb] << 12) + (ia << 6) + jb)
+            hpush((grid[ia][jb] << 12) + (ia << 6) + jb)
             grid[ia][jb] = 3000
         }
-        let next = pq.dequeue().element
+        let next = hpop()
         ans = Math.max(ans, next >> 12)
         i = (next >> 6) & ((1 << 6) - 1)
         j = next & ((1 << 6) - 1)
