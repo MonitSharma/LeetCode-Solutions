5ce72d0a9027e3f55a96d7b1714960f6bf6d18e3
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-09 09:45:43 +0530

    Time: 151 ms (69.70%), Space: 46.5 MB (95.83%) - LeetHub

---
+++ 778-swim-in-rising-water/778-swim-in-rising-water.js
@@ -0,0 +1,19 @@
+const moves = [[1,0],[0,1],[-1,0],[0,-1]]
+
+var swimInWater = function(grid) {
+    let pq = new MinPriorityQueue(),
+        N = grid.length - 1, ans = grid[0][0], i = 0, j = 0
+    while (i < N || j < N) {
+        for (let [a,b] of moves) {
+            let ia = i + a, jb = j + b
+            if (ia < 0 || ia > N || jb < 0 || jb > N || grid[ia][jb] > 2500) continue
+            pq.enqueue((grid[ia][jb] << 12) + (ia << 6) + jb)
+            grid[ia][jb] = 3000
+        }
+        let next = pq.dequeue().element
+        ans = Math.max(ans, next >> 12)
+        i = (next >> 6) & ((1 << 6) - 1)
+        j = next & ((1 << 6) - 1)
+    }
+    return ans
+};
