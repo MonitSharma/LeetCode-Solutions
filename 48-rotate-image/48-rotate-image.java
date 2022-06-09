62777392ad01478f1648af6e4a2ef3c13c5b8d22
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-09 16:04:39 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (63.79%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[][] M) {
+        int n = M.length, depth = n / 2;
+        for (int i = 0; i < depth; i++) {
+            int len = n - 2 * i - 1, opp = n - 1 - i;
+            for (int j = 0; j < len; j++) {
+                int temp = M[i][i+j];
+                M[i][i+j] = M[opp-j][i];
+                M[opp-j][i] = M[opp][opp-j];
+                M[opp][opp-j] = M[i+j][opp];
+                M[i+j][opp] = temp;
+            }
+        }
+    }
+}
