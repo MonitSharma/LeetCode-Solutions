2682ad915836191657af74bc1bf05033d2447b92
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-14 09:35:33 +0530

    Time: 102 ms (86.32%), Space: 44.2 MB (100.00%) - LeetHub

---
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.js
@@ -0,0 +1,15 @@
+var minDistance = function(W1, W2) {
+    let m = W1.length, n = W2.length
+    if (m < n) [W1, W2, m, n] = [W2, W1, n, m]
+    let WA1 = W1.split(""), WA2 = W2.split(""),
+        dpLast = new Uint16Array(n + 1),
+        dpCurr = new Uint16Array(n + 1)
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) 
+            dpCurr[j+1] = WA1[i] === WA2[j]
+                ? dpLast[j] + 1
+                : Math.max(dpCurr[j], dpLast[j+1]);
+        [dpLast, dpCurr] = [dpCurr, dpLast]
+    }
+    return m + n - 2 * dpLast[n] 
+};
