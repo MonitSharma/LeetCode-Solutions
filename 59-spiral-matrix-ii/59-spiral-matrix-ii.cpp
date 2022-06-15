aa7c4ceae48ca3a623dcc3e127c039fb955da65a
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-15 08:27:01 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (18.44%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        char dir = 'E';
+        vector<vector<int>> spiralMatrix(n, vector<int>(n,0));
+        int num=1;
+        int l=0, t=0, r=n-1, d=n-1;
+        while(l<=r && t<=d) {
+            if(dir == 'E') {
+                for(int i=l; i<=r; i++) {
+                    spiralMatrix[t][i] = num++;
+                }
+                t++;
+                dir='S';
+            }
+            else if(dir=='S') {
+                for(int i=t; i<=d; i++) {
+                    spiralMatrix[i][r] = num++;
+                }
+                r--;
+                dir='W';
+            }
+            else if(dir=='W') {
+                for(int i=r; i>=l; i--) {
+                    spiralMatrix[d][i] = num++;
+                }
+                d--;
+                dir='N';
+            }
+            else {
+                for(int i=d; i>=t; i--) {
+                    spiralMatrix[i][l] = num++;
+                }
+                l++;
+                dir='E';
+            }
+        }
+        return spiralMatrix;
+    }
+};
