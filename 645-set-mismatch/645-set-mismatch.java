a0328aae03bef8574241abd1a81394566385f69b
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-21 08:34:25 +0530

    Time: 3 ms (76.25%), Space: 54.9 MB (39.38%) - LeetHub

---
+++ 645-set-mismatch/645-set-mismatch.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int N = nums.length, sum = N * (N + 1) / 2;
+        int[] ans = new int[2];
+        boolean[] seen = new boolean[N+1];
+        for (int num : nums) {
+            sum -= num;
+            if (seen[num]) ans[0] = num;
+            seen[num] = true;
+        }
+        ans[1] = sum + ans[0];
+        return ans;
+    }
+}
