3458a4edc1519e74bde22ab98966203f22b36410
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-21 08:34:42 +0530

    Time: 88 ms (85.85%), Space: 44.2 MB (96.78%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.js
+++ 645-set-mismatch/645-set-mismatch.js
@@ -1,11 +1,9 @@
 var findErrorNums = function(nums) {
-    let N = nums.length, dupe, sum = N * (N + 1) / 2,
-        seen = new Uint8Array(N+1)
-    for (let i = 0; i < N; i++) {
-        let num = nums[i]
-        sum -= num
-        if (seen[num]) dupe = num
-        seen[num]++
-    }
-    return [dupe, sum + dupe]
+    let N = nums.length, ans = [,]
+    for (let i = 0; i < N; i++)
+        nums[(nums[i] - 1) % 10000] += 10000
+    for (let i = 0; i < N; i++)
+        if (nums[i] > 20000) ans[0] = i + 1
+        else if (nums[i] < 10001) ans[1] = i + 1
+    return ans
 };
