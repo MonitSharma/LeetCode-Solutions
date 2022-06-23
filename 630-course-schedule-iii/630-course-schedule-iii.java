68eebe5741af9df09d4de6fa210623917ffb5093
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-23 07:56:44 +0530

    Time: 57 ms (55.00%), Space: 74.7 MB (22.41%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int scheduleCourse(int[][] C) {
+        Arrays.sort(C, (a,b) -> a[1] - b[1]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        int total = 0;
+        for (int[] course : C) {
+            int dur = course[0], end = course[1];
+            if (dur + total <= end) {
+                total += dur;
+                pq.add(dur);
+            } else if (pq.size() > 0 && pq.peek() > dur) {
+                total += dur - pq.poll();
+                pq.add(dur);
+            }
+        }
+        return pq.size();
+    }
+}
