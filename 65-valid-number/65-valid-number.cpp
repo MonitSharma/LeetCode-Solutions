fe6130c5e7d6f03b0c3f77dc716643498540da95
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-24 13:46:59 +0530

    Time: 3 ms (72.07%), Space: 5.9 MB (77.45%) - LeetHub

---
+++ 65-valid-number/65-valid-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isNumber(string S) {
+        bool num = false, exp = false, sign = false, dec = false;
+        for (auto c : S)
+            if (c >= '0' && c <= '9') num = true ;    
+            else if (c == 'e' || c == 'E')
+                if (exp || !num) return false;
+                else exp = true, sign = false, num = false, dec = false;
+            else if (c == '+' || c == '-')
+                if (sign || num || dec) return false;
+                else sign = true;
+            else if (c == '.')
+                if (dec || exp) return false;
+                else dec = true;
+            else return false;
+        return num;
+    }
+};
