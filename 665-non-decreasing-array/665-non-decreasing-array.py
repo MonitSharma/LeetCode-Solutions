daad229ea070f23b10a01c1815ead66660553d68
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-25 09:07:52 +0530

    Time: 184 ms (98.55%), Space: 15.4 MB (5.92%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkPossibility(self, N: List[int]) -> bool:
+        err = 0
+        for i in range(1, len(N)):
+            if N[i] < N[i-1]:
+                if err or (i > 1 and i < len(N) - 1 and N[i-2] > N[i] and N[i+1] < N[i-1]):
+                    return False
+                err = 1
+        return True
