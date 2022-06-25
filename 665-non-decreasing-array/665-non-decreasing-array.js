23479f00478d1ad712ad45260d1ffc13bb82bb37
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-25 09:07:34 +0530

    Time: 95 ms (47.62%), Space: 44.8 MB (45.24%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.js
@@ -0,0 +1,7 @@
+var checkPossibility = function(N) {
+    for (let i = 1, err = 0; i < N.length; i++)
+        if (N[i] < N[i-1])
+            if (err++ || (i > 1 && i < N.length - 1 && N[i-2] > N[i] && N[i+1] < N[i-1]))
+                return false 
+    return true
+};
