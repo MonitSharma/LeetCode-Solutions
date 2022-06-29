91f1514bef8520837d17aa6e2636eabbe73b71fe
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-06-29 14:07:31 +0530

    Time: 13 ms (83.77%), Space: 51.9 MB (61.86%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -1,53 +1,47 @@
-class Codec {
- public:
+public class Codec {
   // Encodes a tree to a single string.
-  string serialize(TreeNode* root) {
-    if (!root)
+  public String serialize(TreeNode root) {
+    if (root == null)
       return "";
 
-    string s;
-    queue<TreeNode*> q{{root}};
+    StringBuilder sb = new StringBuilder();
+    Queue<TreeNode> q = new LinkedList<>(Arrays.asList(root));
 
-    while (!q.empty()) {
-      TreeNode* node = q.front();
-      q.pop();
-      if (node) {
-        s += to_string(node->val) + " ";
-        q.push(node->left);
-        q.push(node->right);
+    while (!q.isEmpty()) {
+      TreeNode node = q.poll();
+      if (node == null) {
+        sb.append("n ");
       } else {
-        s += "n ";
+        sb.append(node.val).append(" ");
+        q.offer(node.left);
+        q.offer(node.right);
       }
     }
 
-    return s;
+    return sb.toString();
   }
 
   // Decodes your encoded data to tree.
-  TreeNode* deserialize(string data) {
-    if (data.empty())
-      return nullptr;
+  public TreeNode deserialize(String data) {
+    if (data.equals(""))
+      return null;
 
-    istringstream iss(data);
-    string word;
-    iss >> word;
-    TreeNode* root = new TreeNode(stoi(word));
-    queue<TreeNode*> q{{root}};
+    final String[] vals = data.split(" ");
+    TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+    Queue<TreeNode> q = new LinkedList<>(Arrays.asList(root));
 
-    while (iss >> word) {
-      TreeNode* node = q.front();
-      q.pop();
-      if (word != "n") {
-        node->left = new TreeNode(stoi(word));
-        q.push(node->left);
+    for (int i = 1; i < vals.length; i += 2) {
+      TreeNode node = q.poll();
+      if (!vals[i].equals("n")) {
+        node.left = new TreeNode(Integer.parseInt(vals[i]));
+        q.offer(node.left);
       }
-      iss >> word;
-      if (word != "n") {
-        node->right = new TreeNode(stoi(word));
-        q.push(node->right);
+      if (!vals[i + 1].equals("n")) {
+        node.right = new TreeNode(Integer.parseInt(vals[i + 1]));
+        q.offer(node.right);
       }
     }
 
     return root;
   }
-};
+}
