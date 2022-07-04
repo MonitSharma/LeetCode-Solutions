1289a55ff8eb3b4a82314ff6d727fe047fd10fea
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-04 20:57:28 +0530

    Time: 280 ms (36.04%), Space: 16.9 MB (37.74%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -1,22 +1,20 @@
-class Solution {
- public:
-  int candy(vector<int>& ratings) {
-    const int n = ratings.size();
-    int ans = 0;
-    vector<int> l(n, 1);
-    vector<int> r(n, 1);
+class Solution:
+  def candy(self, ratings: List[int]) -> int:
+    n = len(ratings)
 
-    for (int i = 1; i < n; ++i)
-      if (ratings[i] > ratings[i - 1])
-        l[i] = l[i - 1] + 1;
+    ans = 0
+    l = [1] * n
+    r = [1] * n
 
-    for (int i = n - 2; i >= 0; --i)
-      if (ratings[i] > ratings[i + 1])
-        r[i] = r[i + 1] + 1;
+    for i in range(1, n):
+      if ratings[i] > ratings[i - 1]:
+        l[i] = l[i - 1] + 1
 
-    for (int i = 0; i < n; ++i)
-      ans += max(l[i], r[i]);
+    for i in range(n - 2, -1, -1):
+      if ratings[i] > ratings[i + 1]:
+        r[i] = r[i + 1] + 1
 
-    return ans;
-  }
-};
+    for a, b in zip(l, r):
+      ans += max(a, b)
+
+    return ans
