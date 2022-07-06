da67836ff8654447f83ba96b866c1f880fd5d7d6
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-06 08:06:00 +0530

    Time: 108 ms (37.33%), Space: 41.9 MB (59.33%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -1,4 +1,7 @@
 var fib = function(n) {
-    let sqrt5 = Math.sqrt(5)
-    return (Math.pow(1 + sqrt5, n) - Math.pow(1 - sqrt5, n)) / Math.pow(2, n) / sqrt5
+    if (n < 2) return n
+    let a = 0, b = 1
+    for (let i = 1; i < n; i++)
+        [a,b] = [b,a+b]
+    return b
 };
