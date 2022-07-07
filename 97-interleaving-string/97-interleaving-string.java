1b3a1a587707d08651197ab1412fa3dc0cde859d
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-07 08:28:57 +0530

    Time: 72 ms (40.18%), Space: 14 MB (73.22%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,26 +1,24 @@
-class Solution {
-  public boolean isInterleave(String s1, String s2, String s3) {
-    final int m = s1.length();
-    final int n = s2.length();
-    if (m + n != s3.length())
-      return false;
+class Solution:
+  def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+    m = len(s1)
+    n = len(s2)
+    if m + n != len(s3):
+      return False
 
-    // dp[i][j] := true if s3[0..i + j) is formed by the interleaving of
-    //             s1[0..i) and s2[0..j)
-    boolean[][] dp = new boolean[m + 1][n + 1];
-    dp[0][0] = true;
+    # dp[i][j] := true if s3[0..i + j) is formed by the interleaving of
+    #             s1[0..i) and s2[0..j)
+    dp = [[False] * (n + 1) for _ in range(m + 1)]
+    dp[0][0] = True
 
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
+    for i in range(1, m + 1):
+      dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
 
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+    for j in range(1, n + 1):
+      dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
 
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        dp[i][j] = dp[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1) ||
-                   dp[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+    for i in range(1, m + 1):
+      for j in range(1, n + 1):
+        dp[i][j] = (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]) or \
+            (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
 
-    return dp[m][n];
-  }
-}
+    return dp[m][n]
