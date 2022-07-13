b8001c3ac78480bdccf0bdd235a06bf4977fe67d
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-13 09:25:56 +0530

    Time: 96 ms (50.65%), Space: 44.3 MB (54.90%) - LeetHub

---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -0,0 +1,14 @@
+var levelOrder = function(root) {
+    let q = [root], ans = []
+    while (q[0]) {
+        let qlen = q.length, row = []
+        for (let i = 0; i < qlen; i++) {
+            let curr = q.shift()
+            row.push(curr.val)
+            if (curr.left) q.push(curr.left)
+            if (curr.right) q.push(curr.right)
+        }
+        ans.push(row)            
+    }
+    return ans
+};
