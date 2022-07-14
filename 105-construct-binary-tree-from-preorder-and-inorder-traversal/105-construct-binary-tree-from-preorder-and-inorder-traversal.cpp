8019d070d2c62f4c4f5aca1deef6172f6392eb30
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-14 10:27:15 +0530

    Time: 19 ms (88.36%), Space: 26.4 MB (28.28%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,18 +1,20 @@
 class Solution {
-    public TreeNode buildTree(int[] P, int[] I) {
-        Map<Integer, Integer> M = new HashMap<>();
-        for (int i = 0; i < I.length; i++)
-            M.put(I[i], i);
-        return splitTree(P, M, 0, 0, I.length-1);
+public:
+    TreeNode* buildTree(vector<int>& P, vector<int>& I) {
+        unordered_map<int, int> M;
+        for (int i = 0; i < I.size(); i++)
+            M[I[i]] = i;
+        return splitTree(P, M, 0, 0, I.size()-1);
     }
 
-    private TreeNode splitTree(int[] P, Map<Integer, Integer> M, int pix, int ileft, int iright) {
-        int rval = P[pix], imid = M.get(rval);
-        TreeNode root = new TreeNode(rval);            
+private:
+    TreeNode* splitTree(vector<int>& P, unordered_map<int, int>& M, int pix, int ileft, int iright) {
+        int rval = P[pix], imid = M[rval];
+        TreeNode* root = new TreeNode(rval);            
         if (imid > ileft)
-            root.left = splitTree(P, M, pix+1, ileft, imid-1);
+            root->left = splitTree(P, M, pix+1, ileft, imid-1);
         if (imid < iright)
-            root.right = splitTree(P, M, pix+imid-ileft+1, imid+1, iright);
+            root->right = splitTree(P, M, pix+imid-ileft+1, imid+1, iright);
         return root;
     }
-}
+};
