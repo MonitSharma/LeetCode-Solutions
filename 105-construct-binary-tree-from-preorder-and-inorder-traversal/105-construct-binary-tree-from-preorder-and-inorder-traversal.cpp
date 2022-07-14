aab524df5c1ad8b3f3a787bd04f45e68b6f7e5cf
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-14 10:27:01 +0530

    Time: 2 ms (99.03%), Space: 42.4 MB (85.07%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode buildTree(int[] P, int[] I) {
+        Map<Integer, Integer> M = new HashMap<>();
+        for (int i = 0; i < I.length; i++)
+            M.put(I[i], i);
+        return splitTree(P, M, 0, 0, I.length-1);
+    }
+
+    private TreeNode splitTree(int[] P, Map<Integer, Integer> M, int pix, int ileft, int iright) {
+        int rval = P[pix], imid = M.get(rval);
+        TreeNode root = new TreeNode(rval);            
+        if (imid > ileft)
+            root.left = splitTree(P, M, pix+1, ileft, imid-1);
+        if (imid < iright)
+            root.right = splitTree(P, M, pix+imid-ileft+1, imid+1, iright);
+        return root;
+    }
+}
