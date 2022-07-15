ba94aab61473dd6b131202986d8d07db3ae9792f
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-15 09:17:38 +0530

    Time: 24 ms (74.49%), Space: 23.1 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,17 +1,18 @@
 class Solution {
-    private int n, m;
-    public int maxAreaOfIsland(int[][] grid) {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
         int ans = 0;
-        n = grid.length;
-        m = grid[0].length;
+        n = grid.size(), m = grid[0].size();
         for (int i = 0; i < n; i++) 
             for (int j = 0; j < m; j++)
-                if (grid[i][j] > 0) ans = Math.max(ans, trav(i, j, grid));
+                if (grid[i][j]) ans = max(ans, trav(i, j, grid));
         return ans;
     }
-    private int trav(int i, int j, int[][] grid) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] < 1) return 0;
+private:
+    int n, m;
+    int trav(int i, int j, vector<vector<int>>& grid) {
+        if (i < 0 || j < 0 || i >= n || j >= m || !grid[i][j]) return 0;
         grid[i][j] = 0;
         return 1 + trav(i-1, j, grid) + trav(i, j-1, grid) + trav(i+1, j, grid) + trav(i, j+1, grid);
     }
-}
+};
