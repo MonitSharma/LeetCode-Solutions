f2ab10620aa6a2926f447301172d29215e1fcf9b
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-17 07:09:49 +0530

    Time: 591 ms (62.50%), Space: 13.9 MB (86.25%) - LeetHub

---
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def kInversePairs(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        M = 1000000007
+        dp = [[0]*(k+1) for _ in range(2)]
+        dp[0][0] = 1
+        for i in range(1, n+1):
+            dp[i%2] = [0]*(k+1)
+            dp[i%2][0] = 1
+            for j in range(1, k+1):
+                dp[i%2][j] = (dp[i%2][j-1] + dp[(i-1)%2][j]) % M
+                if j-i >= 0:
+                    dp[i%2][j] = (dp[i%2][j] - dp[(i-1)%2][j-i]) % M
+        return dp[n%2][k]
