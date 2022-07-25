804a50d083e0776f0e8dcc4f6dd4c696e6894db3
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-25 08:07:14 +0530

    Time: 79 ms (66.50%), Space: 42.4 MB (81.69%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,5 +1,13 @@
-class Solution:
-    def searchRange(self, N: List[int], T: int) -> List[int]:
-        Tleft = bisect_left(N, T)
-        if Tleft == len(N) or N[Tleft] != T: return [-1, -1]
-        return [Tleft, bisect_right(N, T) - 1]
+var searchRange = function(N, T) {
+    const find = (target, arr, left=0, right=arr.length) => {
+        while (left <= right) {
+            let mid = left + right >> 1
+            if (arr[mid] < target) left = mid + 1
+            else right = mid - 1
+        }
+        return left
+    } 
+    let Tleft = find(T, N)
+    if (N[Tleft] !== T) return [-1,-1]
+    return [Tleft, find(T+1, N, Tleft) - 1]
+};
