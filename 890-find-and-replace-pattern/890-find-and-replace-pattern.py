856622398763bb47a6c9dcc4330021222c886803
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-07-29 13:13:45 +0530

    Time: 83 ms (77.50%), Space: 42 MB (100.00%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -0,0 +1,20 @@
+var findAndReplacePattern = function(words, pattern) {
+    let ans = [], codex = new Map()
+    const translate = char => {
+        if (!codex.has(char))
+            codex.set(char, String.fromCharCode(97 + codex.size))
+        return codex.get(char)
+    }
+    const compare = word => {
+        codex.clear()
+        for (let i = 0; i < word.length; i++)
+            if (translate(word[i]) !== cipher[i])
+                return
+        ans.push(word)
+    }
+    let cipher = new Array(pattern.length)
+    for (let i = 0; i < pattern.length; i++)
+        cipher[i] = translate(pattern.charAt(i))
+    words.forEach(compare)
+    return ans
+};
