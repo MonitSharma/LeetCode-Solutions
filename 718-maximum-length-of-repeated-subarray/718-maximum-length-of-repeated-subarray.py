23a41f8d4e479df72b7d85872134e59b419c749f
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-09-20 20:17:19 +0530

    Time: 9013 ms (8.53%), Space: 39.3 MB (50.31%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,18 +1,15 @@
-class Solution {
-  public int findLength(int[] nums1, int[] nums2) {
-    final int m = nums1.length;
-    final int n = nums2.length;
-    int ans = 0;
-    // dp[i][j] := max length of nums1[i:] and nums2[j:]
-    int[][] dp = new int[m + 1][n + 1];
+class Solution:
+  def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+    m = len(nums1)
+    n = len(nums2)
+    ans = 0
+    # dp[i][j] := max length of nums1[i:] and nums2[j:]
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-    for (int i = m - 1; i >= 0; --i)
-      for (int j = n - 1; j >= 0; --j)
-        if (nums1[i] == nums2[j]) {
-          dp[i][j] = dp[i + 1][j + 1] + 1;
-          ans = Math.max(ans, dp[i][j]);
-        }
+    for i in reversed(range(m)):
+      for j in reversed(range(n)):
+        if nums1[i] == nums2[j]:
+          dp[i][j] = dp[i + 1][j + 1] + 1
+          ans = max(ans, dp[i][j])
 
-    return ans;
-  }
-}
+    return ans
