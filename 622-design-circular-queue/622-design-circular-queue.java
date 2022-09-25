34fc17080c1e6ac69100f5051aeea84ab03231fa
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-09-25 13:56:02 +0530

    Time: 134 ms (33.09%), Space: 14.5 MB (85.14%) - LeetHub

---
+++ 622-design-circular-queue/622-design-circular-queue.java
@@ -0,0 +1,24 @@
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.data = [0] * k
+        self.maxSize = k
+        self.head = 0
+        self.tail = -1
+    def enQueue(self, val: int) -> bool:
+        if self.isFull(): return False
+        self.tail = (self.tail + 1) % self.maxSize
+        self.data[self.tail] = val
+        return True
+    def deQueue(self) -> bool:
+        if self.isEmpty(): return False
+        if self.head == self.tail: self.head, self.tail = 0, -1
+        else: self.head = (self.head + 1) % self.maxSize
+        return True
+    def Front(self) -> int:
+        return -1 if self.isEmpty() else self.data[self.head]
+    def Rear(self) -> int:
+        return -1 if self.isEmpty() else self.data[self.tail]
+    def isEmpty(self) -> bool:
+        return self.tail == -1
+    def isFull(self) -> bool:
+        return not self.isEmpty() and (self.tail + 1) % self.maxSize == self.head
