c183433a836d5686aeb213820941d0176cf8f969
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-09-25 13:56:39 +0530

    Time: 11 ms (15.69%), Space: 48.2 MB (49.63%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.java
+++ 622-design-circular-queue/622-design-circular-queue.java
@@ -1,24 +1,44 @@
-class MyCircularQueue:
-    def __init__(self, k: int):
-        self.data = [0] * k
-        self.maxSize = k
-        self.head = 0
-        self.tail = -1
-    def enQueue(self, val: int) -> bool:
-        if self.isFull(): return False
-        self.tail = (self.tail + 1) % self.maxSize
-        self.data[self.tail] = val
-        return True
-    def deQueue(self) -> bool:
-        if self.isEmpty(): return False
-        if self.head == self.tail: self.head, self.tail = 0, -1
-        else: self.head = (self.head + 1) % self.maxSize
-        return True
-    def Front(self) -> int:
-        return -1 if self.isEmpty() else self.data[self.head]
-    def Rear(self) -> int:
-        return -1 if self.isEmpty() else self.data[self.tail]
-    def isEmpty(self) -> bool:
-        return self.tail == -1
-    def isFull(self) -> bool:
-        return not self.isEmpty() and (self.tail + 1) % self.maxSize == self.head
+class ListNode {
+    int val;
+    ListNode next;
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+class MyCircularQueue {
+    int maxSize, size = 0;
+    ListNode head = null, tail = null;
+    public MyCircularQueue(int k) {
+        maxSize = k;
+    }
+    public boolean enQueue(int val) {
+        if (isFull()) return false;
+        ListNode newNode = new ListNode(val, null);
+        if (isEmpty()) head = tail = newNode;
+        else {
+            tail.next = newNode;
+            tail = tail.next;
+        }
+        size++;
+        return true;
+    }
+    public boolean deQueue() {
+        if (isEmpty()) return false;
+        head = head.next;
+        size--;
+        return true;
+    }
+    public int Front() {
+        return isEmpty() ? -1 : head.val;
+    }
+    public int Rear() {
+        return isEmpty() ? -1 : tail.val;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public boolean isFull() {
+        return size == maxSize;
+    }
+}
