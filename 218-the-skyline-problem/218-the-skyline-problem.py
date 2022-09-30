12bdab61079d0806679f0d1a7c3d4cc187cea7c6
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-09-30 20:09:02 +0530

    Time: 310 ms (32.29%), Space: 18.4 MB (99.82%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.py
+++ 218-the-skyline-problem/218-the-skyline-problem.py
@@ -1,56 +1,48 @@
-class Solution {
-  public List<List<Integer>> getSkyline(int[][] buildings) {
-    final int n = buildings.length;
-    if (n == 0)
-      return new ArrayList<>();
-    if (n == 1) {
-      final int left = buildings[0][0];
-      final int right = buildings[0][1];
-      final int height = buildings[0][2];
-      List<List<Integer>> ans = new ArrayList<>();
-      ans.add(new ArrayList<>(Arrays.asList(left, height)));
-      ans.add(new ArrayList<>(Arrays.asList(right, 0)));
-      return ans;
-    }
+class Solution:
+  def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+    n = len(buildings)
+    if n == 0:
+      return []
+    if n == 1:
+      left, right, height = buildings[0]
+      return [[left, height], [right, 0]]
 
-    List<List<Integer>> leftSkyline = getSkyline(Arrays.copyOfRange(buildings, 0, n / 2));
-    List<List<Integer>> rightSkyline = getSkyline(Arrays.copyOfRange(buildings, n / 2, n));
-    return merge(leftSkyline, rightSkyline);
-  }
+    left = self.getSkyline(buildings[:n // 2])
+    right = self.getSkyline(buildings[n // 2:])
+    return self._merge(left, right)
 
-  private List<List<Integer>> merge(List<List<Integer>> left, List<List<Integer>> right) {
-    List<List<Integer>> ans = new ArrayList<>();
-    int i = 0; // left's index
-    int j = 0; // right's index
-    int leftY = 0;
-    int rightY = 0;
+  def _merge(self, left: List[List[int]], right: List[List[int]]) -> List[List[int]]:
+    ans = []
+    i = 0  # left's index
+    j = 0  # right's index
+    leftY = 0
+    rightY = 0
 
-    while (i < left.size() && j < right.size())
-      // choose the point with smaller x
-      if (left.get(i).get(0) < right.get(j).get(0)) {
-        leftY = left.get(i).get(1); // update the ongoing leftY
-        addPoint(ans, left.get(i).get(0), Math.max(left.get(i++).get(1), rightY));
-      } else {
-        rightY = right.get(j).get(1); // update the ongoing rightY
-        addPoint(ans, right.get(j).get(0), Math.max(right.get(j++).get(1), leftY));
-      }
+    while i < len(left) and j < len(right):
+      # choose the powith smaller x
+      if left[i][0] < right[j][0]:
+        leftY = left[i][1]  # update the ongoing leftY
+        self._addPoint(ans, left[i][0], max(left[i][1], rightY))
+        i += 1
+      else:
+        rightY = right[j][1]  # update the ongoing rightY
+        self._addPoint(ans, right[j][0], max(right[j][1], leftY))
+        j += 1
 
-    while (i < left.size())
-      addPoint(ans, left.get(i).get(0), left.get(i++).get(1));
+    while i < len(left):
+      self._addPoint(ans, left[i][0], left[i][1])
+      i += 1
 
-    while (j < right.size())
-      addPoint(ans, right.get(j).get(0), right.get(j++).get(1));
+    while j < len(right):
+      self._addPoint(ans, right[j][0], right[j][1])
+      j += 1
 
-    return ans;
-  }
+    return ans
 
-  private void addPoint(List<List<Integer>> ans, int x, int y) {
-    if (!ans.isEmpty() && ans.get(ans.size() - 1).get(0) == x) {
-      ans.get(ans.size() - 1).set(1, y);
-      return;
-    }
-    if (!ans.isEmpty() && ans.get(ans.size() - 1).get(1) == y)
-      return;
-    ans.add(new ArrayList<>(Arrays.asList(x, y)));
-  }
-}
+  def _addPoint(self, ans: List[List[int]], x: int, y: int) -> None:
+    if ans and ans[-1][0] == x:
+      ans[-1][1] = y
+      return
+    if ans and ans[-1][1] == y:
+      return
+    ans.append([x, y])
