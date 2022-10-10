e18083a008501f182b963811408a49b3021369ad
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-10-10 10:30:46 +0530

    Final Clean Up

--- number of steps to reduce a problem to zero.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def numberOfSteps (self, num: int) -> int:
-        ans = 0
-        while num > 0:
-            if num % 2: num -= 1
-            else: num /= 2
-            ans += 1
-        return ans
