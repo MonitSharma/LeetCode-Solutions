0277c0bac3c23628b69727a2fc4401ad205a4f48
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-10-10 10:26:03 +0530

    Mission Cleanup

--- running sum of 1d array.js
+++
@@ -1,7 +0,0 @@
-var runningSum = function(nums) {
-    let ans = new Array(nums.length)
-    ans[0] = nums[0]
-    for (let i = 1; i < nums.length; i++)
-        ans[i] = ans[i-1] + nums[i]
-    return ans
-};
--- search_insert_position.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-            try:
-                for i, num in enumerate(nums):
-                    if target == num or target < num:
-                        return i
-                return i + 1
-            except:
-                return 0
