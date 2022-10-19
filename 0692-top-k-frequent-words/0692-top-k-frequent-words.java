344d4947df5edf415cf863a8f35c15466e8bb879
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2022-10-19 10:09:38 +0530

    Time: 12 ms (58.40%), Space: 45.8 MB (8.97%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,36 +1,30 @@
-class T {
-  public String word;
-  public int freq;
-  public T(String word, int freq) {
-    this.word = word;
-    this.freq = freq;
-  }
-};
-
 class Solution {
   public List<String> topKFrequent(String[] words, int k) {
+    final int n = words.length;
     List<String> ans = new ArrayList<>();
+    List<String>[] bucket = new List[n + 1];
     Map<String, Integer> count = new HashMap<>();
-    // Words w/ higher frequency and lower alphabetical order are in the bottom
-    // Of the heap, because we'll pop words w/ lower frequency and higher
-    // Alphabetical order if the heap's size > k
-
-    Queue<T> heap = new PriorityQueue<>(
-        (a, b) -> a.freq == b.freq ? b.word.compareTo(a.word) : a.freq - b.freq);
 
     for (final String word : words)
       count.put(word, count.getOrDefault(word, 0) + 1);
 
     for (final String word : count.keySet()) {
-      heap.offer(new T(word, count.get(word)));
-      if (heap.size() > k)
-        heap.poll();
+      final int freq = count.get(word);
+      if (bucket[freq] == null)
+        bucket[freq] = new ArrayList<>();
+      bucket[freq].add(word);
     }
 
-    while (!heap.isEmpty())
-      ans.add(heap.poll().word);
+    for (int freq = n; freq > 0; --freq)
+      if (bucket[freq] != null) {
+        Collections.sort(bucket[freq]);
+        for (final String word : bucket[freq]) {
+          ans.add(word);
+          if (ans.size() == k)
+            return ans;
+        }
+      }
 
-    Collections.reverse(ans);
-    return ans;
+    throw new IllegalArgumentException();
   }
 }
