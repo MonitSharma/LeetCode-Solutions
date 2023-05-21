ef626db089c65d651244dd7dce4fc7dcbde88847
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2023-05-21 10:23:34 +0800

    Added 934

---
+++ 934-shortest-bridge/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        def dfs(i, j):
+            q.append((i, j))
+            grid[i][j] = 2
+            for a, b in pairwise(dirs):
+                x, y = i + a, j + b
+                if 0 <= x < n and 0 <= y < n and grid[x][y] == 1:
+                    dfs(x, y)
+
+        n = len(grid)
+        dirs = (-1, 0, 1, 0, -1)
+        q = deque()
+        i, j = next((i, j) for i in range(n) for j in range(n) if grid[i][j])
+        dfs(i, j)
+        ans = 0
+        while 1:
+            for _ in range(len(q)):
+                i, j = q.popleft()
+                for a, b in pairwise(dirs):
+                    x, y = i + a, j + b
+                    if 0 <= x < n and 0 <= y < n:
+                        if grid[x][y] == 1:
+                            return ans
+                        if grid[x][y] == 0:
+                            grid[x][y] = 2
+                            q.append((x, y))
+            ans += 1
