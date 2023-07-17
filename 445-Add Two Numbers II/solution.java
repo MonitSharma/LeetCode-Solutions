531dfc3dd2937771679cefc4dd1f7895bc597192
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2023-07-17 09:03:14 +0800

    Added 445

---
+++ 445-Add Two Numbers II/solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    Deque<ListNode> stack1 = new ArrayDeque<>();
+    Deque<ListNode> stack2 = new ArrayDeque<>();
+
+    while (l1 != null) {
+      stack1.push(l1);
+      l1 = l1.next;
+    }
+
+    while (l2 != null) {
+      stack2.push(l2);
+      l2 = l2.next;
+    }
+
+    ListNode head = null;
+    int carry = 0;
+
+    while (carry > 0 || !stack1.isEmpty() || !stack2.isEmpty()) {
+      if (!stack1.isEmpty())
+        carry += stack1.pop().val;
+      if (!stack2.isEmpty())
+        carry += stack2.pop().val;
+      ListNode node = new ListNode(carry % 10);
+      node.next = head;
+      head = node;
+      carry /= 10;
+    }
+
+    return head;
+  }
+}
