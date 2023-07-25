cdf3fc6074a16c5b0c14101026e854b3e1f0d2df
Monit Sharma <65262068+MonitSharma@users.noreply.github.com>
2023-07-25 08:29:57 +0800

    Added 852

---
+++ 852-Peak Index in a Mountain Array/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int l=0;
+        int r= arr.size()-1;
+
+        while(l<r){
+            int m= l+ (r-l)/2;
+            if(arr[m]>arr[m+1]){
+                r=m;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return l;
+    
+    }
+};
